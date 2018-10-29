6aa6fd8c3a8e765870578c1537729644c062dd79
EtimeLEE <1015607255@qq.com>
2018-10-29 11:50:57 +0800

    添加

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const chalk = require('chalk')
+const path = require('path')
+const conf = require('./config/defaultConfig')
+const route = require('./helper/route')
+
+class Server{
+  constructor (config) {
+    this.conf = Object.assign({}, conf, config)
+  }
+
+  start() {
+    const server = http.createServer((req, res) => {
+      const filePath = path.join(this.conf.root, req.url)
+      route(req, res, filePath, this.conf)
+    });
+
+    server.listen(this.conf.port, this.conf.host, () => {
+      const addr = `http://${this.conf.host}:${this.conf.port}`
+      console.info(`Server started at ${chalk.green(addr)}`)
+    })
+  }
+}
+
+module.exports = Server
